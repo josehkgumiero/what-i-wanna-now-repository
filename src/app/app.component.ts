27a630f6adc9cde224fa20fafe861a93e6561fb0
jose <jose_hkg@hotmail.com>
2020-04-17 17:11:16 -0300

    first commit: test

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wiwn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'what-i-wanna-now';
+}

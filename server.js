aaf6e65810f9095454913b54303a5e733dd5211c
jose <jose_hkg@hotmail.com>
2020-04-17 17:49:07 -0300

    test: heroku deploy

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/what-i-wanna-now'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/what-i-wanna-now/index.html'));
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
